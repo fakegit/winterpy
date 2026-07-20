8707d30143b33e10ae740e26ab080e8226ed9b5f
lilydjwg <lilydjwg@gmail.com>
2026-07-21 00:04:38 +0800

    pkgarch & nicelogger: modernize
    Co-authored-by: GLM 5.2

--- pylib/nicelogger.py
+++ pylib/nicelogger.py
@@ -15,11 +15,7 @@ class TornadoLogFormatter(logging.Formatter):
     if color:
       import curses
       curses.setupterm()
-      if sys.hexversion < 0x30203f0:
-        fg_color = str(curses.tigetstr("setaf") or
-                   curses.tigetstr("setf") or "", "ascii")
-      else:
-        fg_color = curses.tigetstr("setaf") or curses.tigetstr("setf") or b""
+      fg_color = curses.tigetstr("setaf") or curses.tigetstr("setf") or b""
       self._colors = {
         logging.DEBUG: str(curses.tparm(fg_color, 4), # Blue
                      "ascii"),
@@ -84,7 +80,7 @@ def enable_pretty_logging(level=logging.DEBUG, handler=None, color=None):
         curses.setupterm()
         if curses.tigetnum("colors") > 0:
           color = True
-      except:
+      except Exception:
         import traceback
         traceback.print_exc()
   formatter = TornadoLogFormatter(color=color)
